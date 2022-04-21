84f323a1a061cbedb48e358c38dad277303d78dc
Charles Marques <charles_sjp@yahoo.com.br>
2022-04-21 18:38:18 -0300

    ok

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded());
 
 const pokedex = [
   {
-    id: 001,
+    id: 1,
     nome: "Bulbassaur",
     descricao:
       "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
@@ -18,7 +18,7 @@ const pokedex = [
     imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
   },
   {
-    id: 002,
+    id: 2,
     nome: "Charmander",
     descricao:
       "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
@@ -26,7 +26,7 @@ const pokedex = [
     imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
   },
   {
-    id: 003,
+    id: 3,
     nome: "Butterfree",
     descricao:
       "In battle, it flaps its wings at great speed to release highly toxic dust into the air.",
@@ -34,26 +34,40 @@ const pokedex = [
     imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/012.png",
   },
   {
-    id: 004,
+    id: 4,
     nome: "Nidorino",
     descricao:
       "With a horn that’s harder than diamond, this Pokémon goes around shattering boulders as it searches for a moon stone",
     tipo: "Poison",
     imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/033.png",
-  }
+  },
 ];
-
+let pokemon = undefined;
 //Rotas
 app.get("/", (req, res) => {
-  res.render("index", { pokedex });
+  res.render("index", { pokedex, pokemon });
 });
 
-app.post("/add", (req, res) => {
+app.post("/create", (req, res) => {
   const pokemon = req.body;
-  pokedex.push(pokemon)
-  
+  pokemon.id = pokedex.length + 1;
+  pokedex.push(pokemon);
   res.redirect("/");
-  
+});
+
+app.get("/detalhes/:id", (req, res) => {
+  const id = +req.params.id;
+  pokemon = pokedex.find((pokemon) => pokemon.id === id);
+  res.redirect("/");
+});
+
+app.post("/update/:id", (req, res) => {
+  const id = +req.params.id - 1;
+  const newPokemon = req.body;
+  newPokemon.id = id + 1;
+  pokedex[id] = newPokemon;
+  pokemon = undefined;
+  res.redirect("/");
 });
 
 app.listen(3000, () =>
