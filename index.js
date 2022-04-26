c5d2e39255d366ba6bf9558821b861b80a5bf5f8
Charles Marques <charles_sjp@yahoo.com.br>
2022-04-26 18:12:25 -0300

    config deploy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const res = require("express/lib/response");
 const path = require("path");
 const app = express();
 app.set("view engine", "ejs");
-
+const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded());
 
